c49f5377cb96c1b14d7babf95b05ae86eaab38fb
adfg3314 <adfg3314@naver.com>
2015-09-15 20:45:59 +0900

    0915

---
+++ zzzz/test2.js
@@ -0,0 +1,15 @@
+﻿
+var arrayInt = new Array();
+
+arrayInt.push(3);
+arrayInt.push(8);
+arrayInt.push(213);
+arrayInt.push(213123);
+
+document.write("<br>");
+
+for(var i = 0; i <arrayInt.length; i++) {
+	document.writeln(i + "번 인덱스의 원소 값 : " + arrayInt[i] + "<br>");
+}
+
+document.writeln("arrayInt 배열의 원소 수 : " + arrayInt.length + "<br>");
